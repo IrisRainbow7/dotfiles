b619a99afed17b57abe4cd79e7b406063e77d25a
Irisrainbow7 <a17.bnxk@g.chuo-u.ac.jp>
2020-12-24 00:06:25 +0900

    追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@ ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 cat bashrc_add.txt >> ~/.bashrc
+git clone https://github.com/tmux-plugins/tmux-battery ~/tmux-battery
 source ~/.bashrc
