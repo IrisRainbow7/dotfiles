bce4342411cb427e5ef2fce4b5ed497882e5eedb
平井 航稀 <koki.hirai@hirais-MacBook-Pro.local>
2023-05-17 14:52:07 +0900

    gitconfig追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,6 +2,7 @@
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 cat bashrc_add.txt >> ~/.bashrc
 git clone https://github.com/tmux-plugins/tmux-battery ~/tmux-battery
 source ~/.bashrc
