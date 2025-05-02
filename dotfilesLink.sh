b74b21bdd740daf83ecd91e2f63d8ebe1c6d9a06
Irisrainbow7 <>
2025-05-02 17:58:00 +0900

    fix dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-cat bashrc_add.txt >> ~/.bashrc
-git clone https://github.com/tmux-plugins/tmux-battery ~/tmux-battery
-source ~/.bashrc
+ln -sf ~/dotfiles/.config ~/.config
